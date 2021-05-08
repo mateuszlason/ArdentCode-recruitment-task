cb9d196911f63564255bc9c98eaf5446854da8b9
MateuszLason <crew1997@gmail.com>
2021-05-08 03:23:38 +0200

    added save function featuring Blob constructor

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,33 @@ import "../scss/main.scss";
 
 const buttonImport = document.getElementById("buttonImport");
 const buttonSave = document.getElementById("buttonSave");
+const textArea = document.querySelector(".editor--js");
 buttonImport.addEventListener("click", () => {
   importText();
 });
 
-buttonImport.addEventListener("click", () => {
-  importText();
+buttonSave.addEventListener("click", () => {
+  download();
 });
+
+function download() {
+  const json = { text: textArea.value };
+  const a = document.createElement("a");
+  const file = new Blob([JSON.stringify(json, null, 2)], {
+    type: "application/json",
+  });
+  a.href = URL.createObjectURL(file);
+  a.download = "text.json";
+  a.click();
+}
 
 function importText() {
   const file = document.getElementById("file").files[0];
   const fileReader = new FileReader();
   fileReader.onload = (e) => {
-    const loadedText = JSON.parse(e.target.result);
+    const { text } = JSON.parse(e.target.result);
 
-    document.querySelector(".editor--js").value = loadedText.text;
+    textArea.value = text;
   };
 
   fileReader.readAsText(file, "UTF-8");
